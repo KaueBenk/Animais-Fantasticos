b3b4ef9105baabe185834d8864c3f8ef51ede927
Kauê Benk <kauebenk@gmail.com>
2022-08-30 00:03:19 -0300

    0305-classes-e-atributos

--- scripts/0304-foreach-e-arrow-function.js
+++ scripts/0304-foreach-e-arrow-function.js
@@ -11,15 +11,9 @@ document.querySelectorAll("p").forEach((item) => {
 // Como corrigir os erros abaixo:
 const imgs = document.querySelectorAll("img");
 
-imgs.forEach((item, index) => {
-  console.log(item, index);
-});
+imgs.forEach((item, index) => console.log(item, index));
 
 let i = 0;
-imgs.forEach(() => {
-  console.log(i++);
-});
+imgs.forEach(() => console.log(i++));
 
-imgs.forEach(() => {
-  i++;
-});
+imgs.forEach(() => i++);
---
+++ scripts/0305-classes-e-atributos.js
@@ -0,0 +1,20 @@
+// Adicione a classe ativo a todos os itens do menu
+const menu = document.querySelectorAll(".menu a");
+menu.forEach((item) => {
+  item.classList.add("ativo");
+});
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+menu.forEach((item, index) => {
+  if (index != 0) item.classList.remove("ativo");
+});
+
+// Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll("img");
+imgs.forEach((item, index) => {
+  console.log(index, item.hasAttribute("alt"));
+});
+
+// Modifique o href do link externo no menu
+const linkExterno = document.querySelector('nav a[href^="https://"]');
+linkExterno.href = "https://www.google.com.br/";
