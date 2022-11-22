9b3556f2d158030aca62245a941ce729da6e0995
Kauê Benk <kauebenk@gmail.com>
2022-11-22 17:35:49 -0300

    0502-modules

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initAccordion from "./modules/accordion";
+import initAnimacaoScroll from "./modules/scroll-animacao";
+import initScrollSuave from "./modules/scroll-suave";
+import initTabNav from "./modules/tab-nav";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
