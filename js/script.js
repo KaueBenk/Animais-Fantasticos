a19847e4db52f852f72b7698b0c1797d502420b3
Kauê Benk <kauebenk@gmail.com>
2022-12-20 14:48:50 -0300

    0503-modal

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-import initAccordion from "./modules/accordion";
-import initAnimacaoScroll from "./modules/scroll-animacao";
-import initScrollSuave from "./modules/scroll-suave";
-import initTabNav from "./modules/tab-nav";
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tab-nav.js';
+import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
